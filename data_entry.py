5ed2f9008c0151e4cda4e1dd6226f3a950c850f8
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2024-12-22 05:40:37 +0530

    command lines added

--- data_entry.py
+++ data_entry.py
@@ -4,25 +4,28 @@ from datetime import datetime
 
 
 class CSV:
+    # create and name the scv file
     CSV_FILE = "finance_data.csv"
     
+    # create a function to initialize a new csv file if it desn,t exist
     @classmethod
     def initialize_csv(cls):
         try:
             pd.read_csv(cls.CSV_FILE)
             
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["Date", "Amount", "Category", "Description"])
-            df.to_csv(cls.CSV_FILE, index=False)
-        
-        @classmethod
-        def add_entry(cls, date, amount, category, description):
-            new_entry = {
-                "Date": date,
-                "Amount": amount,
-                "Category": category,
-                "Description": description
-            }
+            df = pd.DataFrame(columns=["Date", "Amount", "Category", "Description"]) # names of the data columns 
+            df.to_csv(cls.CSV_FILE, index=False)# it convert dataframe into csv file
+            
+    # new class method for adding entry to the csv file    
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "Date": date,
+            "Amount": amount,
+            "Category": category,
+            "Description": description
+        }
         
          
 CSV.initialize_csv()
