a914411a36363807b0c778fa3d52e76b9f82771f
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2024-12-22 12:39:53 +0530

    fiinal commit

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def plot_transactions(df):
     )
     
     plt.figure(figsize=(10, 5))
-    plt.plot(income_df.index, income_df["Amount"], label="Income", color="purple", linestyle="-.")
+    plt.plot(income_df.index, income_df["Amount"], label="Income", color="purple")
     plt.plot(expense_df.index, expense_df["Amount"], label="Expense", color="green", linestyle="--")
     plt.legend()
     plt.title("Daily Transactions Income vs Expense")
